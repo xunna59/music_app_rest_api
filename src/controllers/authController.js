98dfd45f8ec135cfdcd661cb21dcb8f5ad514441
xunna <nnaemekauzuegbu59@gmail.com>
2024-08-02 21:27:23 +0100

    updated routes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -85,38 +85,6 @@ const authenticateToken = (req, res, next) => {
 };
 
 
-const authMiddleware = (req, res, next) => {
-    const token = req.headers.authorization?.split(' ')[1]; // Get the token from Authorization header
-
-    if (!token) {
-        return res.status(401).json({ success: false, message: 'No token provided' });
-    }
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).json({ success: false, message: 'Invalid token' });
-        }
-        req.userId = decoded.id; // Attach userId to the request object
-        next();
-    });
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
